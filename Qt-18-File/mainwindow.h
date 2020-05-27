9867ccb9563c5efc93ce6fc9250ab8b2604073a3
macdigia <macdigia@gmail.com>
2020-05-27 15:25:09 +0530

    Qt-18-QFile

---
+++ Qt-18-File/mainwindow.h
@@ -0,0 +1,25 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+
+   void writeFile(QString fileName);
+   void readFile(QString fileName);
+};
+
+#endif // MAINWINDOW_H
