05eb57d86917ed3f32c3637daebabb48d4f9e58a
macdigia <macdigia@gmail.com>
2020-05-27 14:57:41 +0530

    Qt-08-Signals & Slots - 3

---
+++ Qt-08-SignalsAndSlots-3/timerclass.h
@@ -0,0 +1,27 @@
+#ifndef TIMERCLASS_H
+#define TIMERCLASS_H
+
+#include<QTimer>
+#include <QDebug>
+#include<QObject>
+
+class TimerClass : public QObject
+{
+    Q_OBJECT
+
+public:
+    TimerClass();
+
+private slots:
+    void timerSlot();
+
+signals:
+    void timerCustomSignal(int);
+
+private:
+    QTimer *timer;
+
+    int timeCount;
+};
+
+#endif // TIMERCLASS_H
